722c75a0d06865ae0a332cb45d8f7eba4a4dc682
Kalle Pohjapelto <kalle@solvr.no>
2020-10-19 15:44:04 +0200

    API endpoint to fetch general info about the site and pending updates

---
+++ src/WebsiteMonitoring.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Website monitoring plugin for Craft CMS 3.x
+ *
+ * Plugin to monitor pending updates on websites
+ *
+ * @link      https://solvr.no
+ * @copyright Copyright (c) 2020 Kalle Pohjapelto
+ */
+
+namespace solvras\websitemonitoring;
+
+use solvras\websitemonitoring\services\WebsiteMonitoringService as WebsiteMonitoringServiceService;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+use craft\web\UrlManager;
+use craft\events\RegisterUrlRulesEvent;
+
+use yii\base\Event;
+
+/**
+ * Class WebsiteMonitoring
+ *
+ * @author    Kalle Pohjapelto
+ * @package   WebsiteMonitoring
+ * @since     1.0.0
+ *
+ * @property  WebsiteMonitoringServiceService $websiteMonitoringService
+ */
+class WebsiteMonitoring extends Plugin
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * @var WebsiteMonitoring
+     */
+    public static $plugin;
+
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $schemaVersion = '1.0.0';
+
+    /**
+     * @var bool
+     */
+    public $hasCpSettings = false;
+
+    /**
+     * @var bool
+     */
+    public $hasCpSection = true;
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['siteActionTrigger1'] = 'website-monitoring/api';
+            }
+        );
+
+        Event::on(
+            Plugins::class,
+            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+            function (PluginEvent $event) {
+                if ($event->plugin === $this) {
+                }
+            }
+        );
+
+        Craft::info(
+            Craft::t(
+                'website-monitoring',
+                '{name} plugin loaded',
+                ['name' => $this->name]
+            ),
+            __METHOD__
+        );
+    }
+
+    public function getCpNavItem()
+    {
+        $item = parent::getCpNavItem();
+        $item['subnav'] = [
+            'token' => ['label' => 'Create token', 'url' => 'website-monitoring']
+        ];
+        return $item;
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+}
---
+++ src/controllers/ApiController.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Website monitoring plugin for Craft CMS 3.x
+ *
+ * Plugin to monitor pending updates on websites
+ *
+ * @link      https://solvr.no
+ * @copyright Copyright (c) 2020 Kalle Pohjapelto
+ */
+
+namespace solvras\websitemonitoring\controllers;
+
+use solvras\websitemonitoring\WebsiteMonitoring;
+
+use Craft;
+use craft\web\Controller;
+use craft\helpers\DateTimeHelper;
+
+/**
+ * @author    Kalle Pohjapelto
+ * @package   WebsiteMonitoring
+ * @since     1.0.0
+ */
+class ApiController extends Controller
+{
+
+    // Protected Properties
+    // =========================================================================
+
+    /**
+     * @var    bool|array Allows anonymous access to this controller's actions.
+     *         The actions must be in 'kebab-case'
+     * @access protected
+     */
+    protected $allowAnonymous = ['info', 'updates'];
+
+    // Public Methods
+    // =========================================================================
+
+     /**
+     * @return mixed
+     */
+    public function actionCreateToken()
+    {
+        $this->requireAdmin();
+        
+        $currentTime = DateTimeHelper::currentUTCDateTime();
+        $expiryDate = $currentTime->add(new \DateInterval('P10Y'));
+
+        $token = Craft::$app->tokens->createToken('/website-monitoring/api/auth', null, $expiryDate);
+
+        Craft::$app->session->setFlash('tokenCreated', "Token created: ".$token);
+        Craft::$app->session->setNotice("Token created");
+
+        return null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionInfo()
+    {
+        $this->requireToken();
+
+        $result = [
+            'general' => [
+                'php' => [
+                    'version' => craft\helpers\App::phpversion(),
+                ],
+                'cms' => [
+                    'edition' => Craft::$app->getLicensedEditionName(), // Craft::$app->getEditionName()
+                    'license' => Craft::$app->api->getLicenseInfo(),
+                    'email' => craft\helpers\App::mailSettings()
+                ],
+                'plugins' => Craft::$app->plugins->getAllPluginInfo()
+            ],
+            'updates' => Craft::$app->api->getUpdates()
+        ];
+
+        return $this->asJson($result);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionUpdates()
+    {
+        $this->requireToken();
+
+        $result = Craft::$app->api->getUpdates();
+
+        return $this->asJson($result);
+    }
+}
